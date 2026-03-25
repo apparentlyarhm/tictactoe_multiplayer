c46357a0ac67de85133b315e5c65bf5d3c0c4550
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-03-26 01:02:13 +0530

    backend: add primary dockerfile

---
+++ backend/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+echo "[INIT] Starting Nakama entrypoint..."
+echo "[INIT] Waiting for DB"
+
+until /nakama/nakama migrate up --database.address "$NAKAMA_DATABASE_ADDRESS"
+do
+  echo "[INIT] DB not ready, retrying in 2s..."
+  sleep 2
+done
+
+echo "[INIT] Migration complete, starting Nakama..."
+
+exec /nakama/nakama \
+  --database.address "$NAKAMA_DATABASE_ADDRESS" \
+  --logger.level "INFO" \
+  --session.token_expiry_sec 7200 \
+  --session.refresh_token_expiry_sec 7200 \
+  --socket.server_key "$SERVER_KEY" \
+  --console.password "$CONSOLE_PASSWORD"
